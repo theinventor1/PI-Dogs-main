5807eda2d6c6b100a7a1b4adf35cfc3ef7e35283
theinventor1 <37818640+theinventor1@users.noreply.github.com>
2023-05-31 21:49:54 -0400

    hasta aqui anda
    hasta aqui anda

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import Card from "../Card/Card";
 import {Conx } from './styled.Cards.js';
  function Cards(props) {  
+  
+  /**redux estado global , disponible para los componentes que lo necesiten */
+  /**redux nos entrega un un paquete de informacion en el estado global , que queda a 
+   * disposicion de cualquier parte de la aplicacion que lo necesite
+    EL ESTADO GLOBAL ES UN OBJETO  
+    { perros :[arr de perros] }
+   reducer:  el unico que puede cambiar cosas em el ESTADO GLOBAL es el reducer. 
+   Cards (conteneder de card) va a estar mirando el estado global  
+    */
+
   /**paginacion */
   const { losperros, onClose , currentPage, cardsPerPage  } = props;
   // Calcula el índice inicial y final de las tarjetas a mostrar según la página actual
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,12 +1,23 @@
 import { ADD_RAZ, REM_RAZ, FILT_RAZ, ORD_RAZ } from "./action-types";
 import { ADD_TMP, REM_TMP, FILT_TMP, ORD_TMP } from "./action-types";
 
+const perrosaux = [];
+
+
 const initialState = {
-    todoPerros:  []
+    perros2:  [{"weight":{"imperial":"6 - 13","metric":"3 - 6"},"height":{"imperial":"9 - 11.5","metric":"23 - 29"},"id":1,"name":"Palta","bred_for":"Small rodent hunting, lapdog","breed_group":"Toy","life_span":"10 - 12 years","temperament":"Stubborn, Curious, Playful, Adventurous, Active, Fun-loving","origin":"Germany, France","reference_image_id":"BJa4kxc4X","urlimagen":"https://cdn2.thedogapi.com/images/BJa4kxc4X.jpg"},
+    {"weight":{"imperial":"6 - 13","metric":"3 - 6"},"height":{"imperial":"9 - 11.5","metric":"23 - 29"},"id":2,"name":"trok","bred_for":"Small rodent hunting, lapdog","breed_group":"Toy","life_span":"10 - 12 years","temperament":"Stubborn, Curious, Playful, Adventurous, Active, Fun-loving","origin":"Germany, France","reference_image_id":"BJa4kxc4X","urlimagen":"https://cdn2.thedogapi.com/images/BJa4kxc4X.jpg"},
+    {"weight":{"imperial":"6 - 13","metric":"3 - 6"},"height":{"imperial":"9 - 11.5","metric":"23 - 29"},"id":3,"name":"postre3","bred_for":"Small rodent hunting, lapdog","breed_group":"Toy","life_span":"10 - 12 years","temperament":"Stubborn, Curious, Playful, Adventurous, Active, Fun-loving","origin":"Germany, France","reference_image_id":"BJa4kxc4X","urlimagen":"https://cdn2.thedogapi.com/images/BJa4kxc4X.jpg"}],
 }
 
+/**reducer es quien sabe que hacer en el estado global  */
 const reducer = (state = initialState, action) => {
- console.log('Reducer....')
+ console.log('Reducer es una funcion....');
+ switch(action.type){
+  default:
+   return { ...state   }  /**una copia del estado */
+ }
+
 }
 
 export default reducer
